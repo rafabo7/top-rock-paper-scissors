3d6511d48f0201e4edeff808f139dbc08587bb03
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-01-31 19:47:12 +0100

    Add JS features for new version. Add a score object-like to store the score. Add the logic to change the score with every round. Add the reference to the document nodes and the event listeners (which may need more work)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,6 +18,10 @@ function getComputerChoice () {
                 }
 }
 
+let score = {
+    'computer' : 0,
+    'human' : 0
+}
 
 function playRound (playerSelection, computerSelection){
     let result;
@@ -28,16 +32,23 @@ function playRound (playerSelection, computerSelection){
         result = "You won!"
     } else if (playerSelection == "scissors" && computerSelection == "paper"){
         result = "You won!"
+        score['human'] += 1
     } else if (playerSelection == computerSelection){
         result = "That's a tie!"
     }
     else {
         result = "You lose!"
+        score['computer'] += 1
     }
     return result
 }
 
-// for (let i = 0; i < 5; i++){
-//     let playerSelection = prompt("Welcome to the game. What is your choice?")
-//     console.log(playRound(playerSelection, getComputerChoice()))
-// }
+const rockBtn = document.querySelector('#rock')
+const paperBtn = document.querySelector('#paper')
+const scissorBtn = document.querySelector('#scissors')
+const scoreBoard = document.querySelector('#scoreboard')
+
+rockBtn.addEventListener('click', playRound('rock', getComputerChoice()))
+paperBtn.addEventListener('click', playRound('paper', getComputerChoice()))
+scissorBtn.addEventListener('click', playRound('scissors', getComputerChoice()))
+
